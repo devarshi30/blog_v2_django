b9cfab5b47548624e2f345c3eef0522ff5ffb664
yash-2115 <mailforyash21@gmail.com>
2021-06-25 10:26:32 +0530

    improved file structure

--- theblog/views.py
+++ theblog/views.py
@@ -15,11 +15,11 @@ def LikeView(request, pk):
 		post.likes.add(request.user)
 		liked = True
 	
-	return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+	return HttpResponseRedirect(reverse('theblog/article-detail', args=[str(pk)]))
 
 class HomeView(ListView):
 	model = Post
-	template_name = 'home.html'
+	template_name = 'theblog/home.html'
 	cats = Category.objects.all()
 	ordering = ['-post_date']
 
@@ -31,17 +31,17 @@ class HomeView(ListView):
 
 def CategoryListView(request):
 	cat_menu_list = Category.objects.all()
-	return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+	return render(request, 'theblog/category_list.html', {'cat_menu_list':cat_menu_list})
 
 
 
 def CategoryView(request, id):
 	category_posts = Post.objects.filter(category=id)
-	return render(request, 'categories.html', {'cats':id, 'category_posts':category_posts})
+	return render(request, 'theblog/categories.html', {'cats':id, 'category_posts':category_posts})
 
 class ArticleDetailView(DetailView):
 	model = Post
-	template_name = 'article_details.html'
+	template_name = 'theblog/article_details.html'
 
 	def get_context_data(self, *args, **kwargs):
 		cat_menu = Category.objects.all()
@@ -62,12 +62,12 @@ class ArticleDetailView(DetailView):
 class AddPostView(CreateView):
 	model = Post
 	form_class = PostForm
-	template_name = 'add_post.html'
+	template_name = 'theblog/add_post.html'
 
 class AddCommentView(CreateView):
 	model = Comment
 	form_class = CommentForm
-	template_name = 'add_comment.html'
+	template_name = 'theblog/add_comment.html'
 	def form_valid(self, form):
 		form.instance.post_id = self.kwargs['pk']
 		return super().form_valid(form)
@@ -76,15 +76,15 @@ class AddCommentView(CreateView):
 
 class AddCategoryView(CreateView):
 	model = Category
-	template_name = 'add_category.html'
+	template_name = 'theblog/add_category.html'
 	fields = '__all__'
 
 class UpdatePostView(UpdateView):
 	model = Post
 	form_class = EditForm
-	template_name = 'update_post.html'
+	template_name = 'theblog/update_post.html'
 
 class DeletePostView(DeleteView):
 	model = Post
-	template_name = 'delete_post.html'
+	template_name = 'theblog/delete_post.html'
 	success_url = reverse_lazy('home')
